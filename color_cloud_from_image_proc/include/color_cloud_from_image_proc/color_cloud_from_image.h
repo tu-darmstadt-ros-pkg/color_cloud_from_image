7b98987f20788166e17b31e7428e58aee4cb4ec9
Martin Oehler <martin.sven.oehler@gmail.com>
2017-06-21 23:29:30 +0200

    implementation

--- color_cloud_from_image_proc/include/color_cloud_from_image_proc/color_cloud_from_image.h
+++ color_cloud_from_image_proc/include/color_cloud_from_image_proc/color_cloud_from_image.h
@@ -6,28 +6,43 @@
 #include <Eigen/Eigen>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/Image.h>
+#include <image_transport/image_transport.h>
+
+#include <functional>
+
+//#include <aslam/cameras/CameraGeometry.hpp>
+//#include <aslam/cameras/RadialTangentialDistortion.hpp>
+//#include <aslam/cameras/OmniProjection.hpp>
+
+#include <aslam/cameras.hpp>
+
 
 namespace color_cloud_from_image {
 
-  struct IntrinsicCalibration {};
+  struct IntrinsicCalibration {
+    double xi, fu, fv, cu, cv, ru, rv;
+    double k1, k2, p1, p2;
+  };
 
   struct CameraModel{
     std::string name;
-    std::string frame_id;
     IntrinsicCalibration calibration;
-    sensor_msgs::Image last_image_;
+    sensor_msgs::ImageConstPtr last_image;
+    boost::shared_ptr<aslam::cameras::CameraGeometryBase> camera;
   };
 
 
   class ColorCloudFromImage {
   public:
+    ColorCloudFromImage();
     // add camera with name and calibration
     // create aslam cam instance for projection
-    void addCamera(std::string name, const IntrinsicCalibration& calibration);
+    void addCamera(std::string name, std::string topic, const IntrinsicCalibration& calibration);
+    void addCamera(std::string name, std::string topic, const ros::NodeHandle& nh);
     IntrinsicCalibration loadCalibration(const ros::NodeHandle& nh);
   private:
     // save images for respective cam
-    void imageCB(std::string cam_name, const sensor_msgs::ImageConstPtr& image_ptr);
+    void imageCallback(std::string cam_name, const sensor_msgs::ImageConstPtr& image_ptr);
     /* on new pc:
      * 1. iterate over each point
      * 2. transform point to cam frame
@@ -35,14 +50,17 @@ namespace color_cloud_from_image {
      * 4. save pixel color for point
      * 5. republish cloud
      */
-    void pointcloudCB(const sensor_msgs::PointCloud2ConstPtr& cloud_ptr);
+    void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_ptr);
 
     void worldToColor(const Eigen::Vector3d& point3d);
-    Eigen::Vector2d worldToCam(const Eigen::Vector3d& point3d);
 
-
+    ros::NodeHandle nh_;
     sensor_msgs::PointCloud2 last_cloud_;
-    std::vector<CameraModel> cameras_;
+    std::map<std::string, CameraModel> cameras_;
+
+    ros::Subscriber cloud_sub_;
+    std::map<std::string, image_transport::Subscriber> cam_subs_;
+    ros::Publisher cloud_pub_;
   };
 }
 
--- color_cloud_from_image_proc/src/color_cloud_from_image.cpp
+++ color_cloud_from_image_proc/src/color_cloud_from_image.cpp
@@ -2,4 +2,65 @@
 
 namespace color_cloud_from_image {
 
+using namespace aslam::cameras;
+
+ColorCloudFromImage::ColorCloudFromImage()
+  : nh_(ros::NodeHandle()) {
+
+
+  cloud_sub_ = nh_.subscribe("cloud", 10, &ColorCloudFromImage::cloudCallback, this);
+  cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("colored_cloud", 1000, false);
+}
+
+IntrinsicCalibration ColorCloudFromImage::loadCalibration(const ros::NodeHandle& nh) {
+
+}
+
+void ColorCloudFromImage::addCamera(std::string name, std::string topic, const ros::NodeHandle& nh) {
+  addCamera(name, topic, loadCalibration(nh));
+}
+
+
+void ColorCloudFromImage::addCamera(std::string name, std::string topic, const IntrinsicCalibration& calibration) {
+  image_transport::ImageTransport it(nh_);
+
+  std::pair<std::string, image_transport::Subscriber> entry(name, it.subscribe(topic, 1, boost::bind(&ColorCloudFromImage::imageCallback, this, name, _1)));
+  cam_subs_.emplace(entry);
+
+  RadialTangentialDistortion distortion(calibration.k1, calibration.k2, calibration.p1, calibration.p2);
+  OmniProjection<RadialTangentialDistortion> projection(calibration.xi, calibration.fu, calibration.fv, calibration.cu, calibration.cv, calibration.ru, calibration.rv, distortion);
+
+  CameraModel model;
+  model.name = name;
+  model.camera.reset(new CameraGeometry<OmniProjection<RadialTangentialDistortion>, GlobalShutter, NoMask>(projection));
+  std::pair<std::string, CameraModel> entry2(name, model);
+  cameras_.emplace(entry2);
+}
+
+
+void ColorCloudFromImage::cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_ptr) {
+
+}
+
+void ColorCloudFromImage::imageCallback(std::string cam_name, const sensor_msgs::ImageConstPtr& image_ptr) {
+  try {
+    cameras_.at(cam_name).last_image = image_ptr;
+  } catch (std::out_of_range) {
+    ROS_ERROR_STREAM("Could not find cam " << cam_name);
+  }
+}
+
+
+void ColorCloudFromImage::worldToColor(const Eigen::Vector3d& point3d) {
+  bool found_color = false;
+  for (std::map<std::string, CameraModel>::iterator c = cameras_.begin(); c != cameras_.end() && !found_color; ++c) {
+    boost::shared_ptr<aslam::cameras::CameraGeometryBase> camera = c->second.camera;
+    Eigen::VectorXd pixel(2);
+    if (camera->vsEuclideanToKeypoint(point3d, pixel)) {
+      //TODO somehow get pixel color from last image of cam and return that color
+      found_color = true;
+    }
+  }
+}
+
 }
